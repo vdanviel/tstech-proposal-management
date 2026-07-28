eef96fe3cdd6d407a09d50b42404578b8f58aae0
vdanviel <victordn.araujo@gmail.com>
2026-07-27 22:23:01 -0300

    init

---
+++ database/migrations/2026_07_28_001726_create_proposal_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('proposal', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->references('id')->on('client');
+            $table->string('product');
+            $table->double('monthly_value');
+            $table->enum('status', ['DRAFT', 'SUBMITTED', 'APPROVED', 'REJECTED', 'CANCELED']);
+            $table->enum('origin', ['APP', 'SITE', 'API']);
+            $table->integer('version');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('proposal');
+    }
+};
