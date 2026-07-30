8e38a0c341c57a0ddebc81a4961d15b6651d56a9
vdanviel <victordn.araujo@gmail.com>
2026-07-30 00:11:56 -0300

    config: alinha configuração para sistema em api; config: adiciona configuração de cors;

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -15,7 +14,29 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
-        );
+        $exceptions->render(function(Throwable $e){
+
+            $excArr = explode('\\', $e::class);
+            $className = $excArr[count($excArr) - 1];
+
+            //caso a exceção for de SQL recuperar so mensagem e não detalhes pois há dados sensíveis
+            $message = $e->getMessage();
+            if ($className === "QueryException") {
+                $message = trim(explode("(", $message)[0]);
+            }
+
+            return response()->json(
+                [
+                    'exception' => $className,
+                    'error' => $message,
+                ],
+                match (true) {
+                    $e instanceof \Illuminate\Validation\ValidationException => 422,
+                    $e instanceof \Illuminate\Auth\AuthenticationException => 401,
+                    $e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException => 404,
+                    $e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException => 404,
+                    default => 500,
+                }
+            );
+        });
     })->create();
