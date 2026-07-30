170f9949f5694fedb2bd88762bde8dbd4cf025bd
vdanviel <victordn.araujo@gmail.com>
2026-07-30 00:25:19 -0300

    chore: factory do cliente no laravel

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Table;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Table('client')]
@@ -11,6 +12,7 @@ class Client extends Model
 {
 
     use SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'name',
