509b25d54b32eb584ea4c190a7c322bf24af4628
Victor Daniel <99292901+vdanviel@users.noreply.github.com>
2026-08-06 13:47:39 -0300

    feat(client): adiciona middleware de autenticação para rotas de cliente

--- routes/api/v1/client.php
+++ routes/api/v1/client.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\V1\ClientController;
 use App\Http\Middleware\IdempotencyMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/clientes', [ClientController::class, 'store'])->middleware([IdempotencyMiddleware::class]);
-Route::get('/clientes/{id}', [ClientController::class, 'show']);
+Route::post('/clientes', [ClientController::class, 'store'])->middleware([IdempotencyMiddleware::class, 'auth:sanctum']);
+Route::get('/clientes/{id}', [ClientController::class, 'show'])->middleware('auth:sanctum');
